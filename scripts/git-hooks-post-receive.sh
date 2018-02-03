c3c2151817d8c902e003cae304b11b3c0c85e975
Shinsuke Nishio <nishio@densan-labs.net>
2018-02-04 03:13:18 +0900

    fix git script

--- scripts/git-hooks-post-receive.sh
+++ scripts/git-hooks-post-receive.sh
@@ -4,10 +4,10 @@ echo "--- DevCast Article Deploy ---"
 echo ""
 
 echo "1/3 Fetch Devcast Repo"
-cd /git/workdir/devcast
-git fetch origin
-git reset --hard origin/master
-git checkout origin/master
+rm -rf /git/workdir/devcast
+cd /git/workdir
+git clone /git/devcast
+
 echo "2/3 Import Repo"
 cd /app/current; RAILS_ENV=production SECRET_KEY_BASE=a REPO_DIR="/git/workdir/devcast" REPO_NAME="devcast"  bundle exec rake repo:import
 echo "3/3 Import Complete"
